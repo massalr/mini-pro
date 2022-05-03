3ab387103e965df55c0aed3231421a0a01a2e241
massalr <lookingforyourname@gmail.com>
2022-05-03 18:28:07 +0700

    madlibs

---
+++ madlibs.py
@@ -0,0 +1,12 @@
+nama = input("nama : ")
+kata_sifat_1 = input("kata sifat 1 : ")
+kata_sifat_2 = input("kata sifat 2 : ")
+angka = input("masukan angka : ")
+kata_sifat_3 = input("kata sifat 3 : ")
+makanan = input("makanan : ")
+nama_laki_laki = input("masukan nama laki-laki : ")
+print("")
+madlibs = f"sayangku {nama} \nselamat ulang tahun ya {kata_sifat_1} semoga kamu jadi semakin {kata_sifat_2}, aku berdoa semoga di usia kamu yang ke {angka} ini bisa jadi tambah dewasa dan tidak {kata_sifat_3} lagi\
+ dan aku tunggu traktiran makan {makanan} yaa {nama_laki_laki} sayang kamu"
+
+print(madlibs)
