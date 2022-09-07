9e7d2f284160d5c794746aa62c6959a817a352a4
massalr <lookingforyourname@gmail.com>
2022-09-07 23:22:36 +0700

    no change

--- madlibs.py
+++ madlibs.py
@@ -6,7 +6,7 @@ kata_sifat_3 = input("kata sifat 3 : ")
 makanan = input("makanan : ")
 nama_laki_laki = input("masukan nama laki-laki : ")
 print("")
-madlibs = f"this blank {nama} \nthis blank {kata_sifat_1} this blank {kata_sifat_2}, this blank {angka} ini bisa jadi tambah dewasa dan tidak {kata_sifat_3} lagi\
+madlibs = f"this blank {nama} \nthis blank {kata_sifat_1} this blank {kata_sifat_2}, this blank {angka} this blank {kata_sifat_3} lagi\
  this blank {makanan} yaa {nama_laki_laki} binatang"
 
 print(madlibs)
