64f8bbfc33828470707f02a46ac433ce36cd32c5
massalr <lookingforyourname@gmail.com>
2022-09-07 23:21:02 +0700

    no change

--- madlibs.py
+++ madlibs.py
@@ -6,7 +6,7 @@ kata_sifat_3 = input("kata sifat 3 : ")
 makanan = input("makanan : ")
 nama_laki_laki = input("masukan nama laki-laki : ")
 print("")
-madlibs = f"sayangku {nama} \nselamat ulang tahun ya {kata_sifat_1} semoga kamu jadi semakin {kata_sifat_2}, aku berdoa semoga di usia kamu yang ke {angka} ini bisa jadi tambah dewasa dan tidak {kata_sifat_3} lagi\
- dan aku tunggu traktiran makan {makanan} yaa {nama_laki_laki} sayang kamu"
+madlibs = f"this blank {nama} \nthis blank {kata_sifat_1} this blank {kata_sifat_2}, this blank {angka} ini bisa jadi tambah dewasa dan tidak {kata_sifat_3} lagi\
+ this blank {makanan} yaa {nama_laki_laki} binatang"
 
 print(madlibs)
